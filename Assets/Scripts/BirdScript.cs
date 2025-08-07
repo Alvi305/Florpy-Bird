20f6e4f377d14fc5c75b262648e88200da581577
Alvi305 <alvirahman305@gmail.com>
2025-08-07 11:14:33 +0800

    Remove Unity Library and Temp files from tracking
    - Removed all Library/ files (32,000+ generated files)
    - Removed all Temp/ files (build cache files)
    - Only track essential source code and assets
    - Updated BirdScript.cs
    
    This reduces repo size significantly and follows Unity best practices.

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -3,20 +3,28 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public  Rigidbody2D myRigidBody;
+    public Rigidbody2D myRigidBody;
     public float flapStrength;
     public LogicScript logic;
+    public bool birdIsAlive = true;
+
     void Start()
     {
-        logic = 
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidBody.linearVelocity = Vector2.up * flapStrength;
         }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
